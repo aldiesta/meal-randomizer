c43c95b5c5d16f18f58d65172ad816c74989dca6
Austin <austin.x.diesta@kohls.com>
2024-07-19 16:13:50 -0500

    adjusted meal cards to be same size

--- src/components/MealCard/MealCard.jsx
+++ src/components/MealCard/MealCard.jsx
@@ -5,43 +5,44 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import './MealCard.scss';
 
 const MealCard = ({ image, title, description }) => {
-	return (
-		<div className="meal-card">
-			<Card className='card' sx={{ maxWidth: 300 }}>
-				<CardMedia className='card-media'
-					sx={{ height: 100 }}
-					image={image}
-					title={title} 
-				/>
-				<CardContent>
-					<Typography gutterBottom variant="h5" component="div">
-						{title}
-					</Typography>
-					<Typography variant="body2" color="text.secondary">
-						{description}
-					</Typography>
-				</CardContent>
-				<CardActions>
-					<Button 
-						size="small"
-						onClick={() => {
-							alert('Show Recipe');
-						}}
-					>
-						Show Recipe
-					</Button>
-				</CardActions>
-			</Card>
-		</div>
-	);
-}
+  return (
+    <div className="meal-card">
+      <Card className='card'>
+        <CardMedia
+          className='card-media'
+          image={image}
+          title={title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button 
+            size="small"
+            onClick={() => {
+              alert('Show Recipe');
+            }}
+          >
+            Show Recipe
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
 
 MealCard.propTypes = {
-	image: PropTypes.string.isRequired,
-	title: PropTypes.string.isRequired,
-	description: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
 };
 
 export default MealCard;
