98ef255a4ffa0f4188342fa7b841778aa3e15c85
Austin <austin.x.diesta@kohls.com>
2024-10-31 15:55:26 -0500

    feat: separated meal data

---
+++ src/components/MainBody/mealData.js
@@ -0,0 +1,99 @@
+const initialMeals = [
+    {
+        image: 'src/assets/images/chicken-parmesan.jpg',
+        title: 'Chicken Parmesan',
+        description: 'Italian',
+    },
+    {
+        image: 'src/assets/images/sushi.jpg',
+        title: 'Sushi',
+        description: 'Japanese',
+    },
+    {
+        image: 'src/assets/images/chili.jpg',
+        title: 'Chili',
+        description: 'American',
+    },
+    {
+        image: 'src/assets/images/pot-roast.jpg',
+        title: 'Pot Roast',
+        description: 'American',
+    },
+    {
+        image: 'src/assets/images/fried-rice.jpg',
+        title: 'Fried Rice',
+        description: 'Chinese',
+    },
+    {
+        image: 'src/assets/images/fajitas.jpg',
+        title: 'Fajitas',
+        description: 'Mexican',
+    },
+    {
+        image: 'src/assets/images/tacos.jpg',
+        title: 'Tacos',
+        description: 'Mexican',
+    },
+    {
+        image: 'src/assets/images/stir-fry.jpg',
+        title: 'Stir Fry',
+        description: 'Chinese',
+    },
+    {
+        image: 'src/assets/images/spaghetti.jpg',
+        title: 'Spaghetti',
+        description: 'Italian',
+    },
+    {
+        image: 'src/assets/images/carbonara.jpg',
+        title: 'Carbonara',
+        description: 'Italian',
+    },
+    {
+        image: 'src/assets/images/brats.jpg',
+        title: 'Brats',
+        description: 'American',
+    },
+    {
+        image: 'src/assets/images/burgers.jpg',
+        title: 'Burgers',
+        description: 'American',
+    },
+    // {
+    //     image: 'src/assets/images/bbq-ribs.jpg',
+    //     title: 'BBQ Ribs',
+    //     description: 'American',
+    // },
+    // {
+    //     image: 'src/assets/images/pulled-pork.jpg',
+    //     title: 'Pulled Pork',
+    //     description: 'American',
+    // },
+    // {
+    //     image: 'src/assets/images/mac-and-cheese.jpg',
+    //     title: 'Mac and Cheese',
+    //     description: 'American',
+    // },
+    // {
+    //     image: 'src/assets/images/pizza.jpg',
+    //     title: 'Pizza',
+    //     description: 'Italian',
+    // },
+    // {
+    //     image: 'src/assets/images/pho.jpg',
+    //     title: 'Pho',
+    //     description: 'Vietnamese',
+    // },
+    // {
+    //     image: 'src/assets/images/bahn-mi.jpg',
+    //     title: 'Bahn Mi',
+    //     description: 'Vietnamese',
+    // },
+    // {
+    //     image: 'src/assets/images/paella.jpg',
+    //     title: 'Paella',
+    //     description: 'Spanish',
+    // },
+];
+
+export default initialMeals
